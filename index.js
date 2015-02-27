55941a2a8dd37d5473aaf2393e89fe86152d7b2b
Juho Vepsalainen <bebraw@gmail.com>
2015-02-27 17:43:04 +0200

    Add basic cli interface
    `ghost2antwar -i <json> -o <directory>`
    
    Closes #1.

--- index.js
+++ index.js
@@ -1,9 +1,46 @@
+#!/usr/bin/env node
 'use strict';
+var fs = require('fs');
+
+var program = require('commander');
+
 var convert = require('./lib/convert');
+var version = require('./package.json').version;
 
 
 main();
 
 function main() {
-    convert();
+    program.version(version).
+        option('-i, --input <json>', 'Input JSON').
+        option('-o --output <directory>', 'Output directory').
+        parse(process.argv);
+
+    var input = program.input;
+    var output = program.output;
+
+    if(!input) {
+        return console.error('missing input');
+    }
+
+    if(!output) {
+        return console.error('missing output');
+    }
+
+    fs.readFile(input, {
+        encoding: 'utf8',
+    }, function(err, data) {
+        if(err) {
+            return console.error(err);
+        }
+        var json;
+
+        try {
+            json = JSON.parse(data);
+
+            convert(json, output);
+        } catch(e) {
+            return console.error('failed to parse JSON');
+        }
+    });
 }
